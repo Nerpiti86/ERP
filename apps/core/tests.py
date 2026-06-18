b4942168983000a4a1ea07234e08c2648836687c
Nicolas Nerpiti <nicolasnerpiti@gmail.com>
2026-06-18 00:27:12 -0300

    Agregar accesos al admin desde la home

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -26,3 +26,16 @@ class HomeTests(TestCase):
         self.assertContains(response, "Sucursales")
         self.assertContains(response, "Sistema activo")
         self.assertContains(response, "Empresa")
+
+    def test_home_muestra_accesos_al_admin_del_nucleo(self):
+        response = self.client.get(reverse("core:home"))
+
+        self.assertContains(response, "Panel admin")
+        self.assertContains(response, "Ver empresas")
+        self.assertContains(response, "Ver sucursales")
+        self.assertContains(response, "Ver ejercicios")
+        self.assertContains(response, "Ver períodos")
+        self.assertContains(response, reverse("admin:nucleo_empresa_changelist"))
+        self.assertContains(response, reverse("admin:nucleo_sucursal_changelist"))
+        self.assertContains(response, reverse("admin:nucleo_ejerciciofiscal_changelist"))
+        self.assertContains(response, reverse("admin:nucleo_periodocontable_changelist"))
